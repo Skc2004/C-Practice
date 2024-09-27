6419b65fa3b234003fb7d95daa14e377d04b1a58
Satyam Kumar Choudhary <choudharysatyam951478@gmail.com>
2024-09-27 09:34:19 +0530

    Adding comments 27/09/24

--- DSA/JobSchedulingUsingQueue.c
+++ DSA/JobSchedulingUsingQueue.c
@@ -1,18 +1,22 @@
+// Including necessary libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// Defining a structure Job to store information about job
 typedef struct Job {
     int jobId;
     char jobName[50];
     struct Job* next;
 } Job;
 
+// Defining a structure Queue for the implementation of the scheduling
 typedef struct Queue {
     Job* front;
     Job* rear;
 } Queue;
 
+// Function to create a Queue
 Job* createJob(int id, char* name) {
     Job* newJob = (Job*)malloc(sizeof(Job));
     newJob->jobId = id;
@@ -21,14 +25,18 @@ Job* createJob(int id, char* name) {
     return newJob;
 }
 
+// Function to intialize the formed queue
 void initializeQueue(Queue* q) {
     q->front = q->rear = NULL;
 }
 
+// Function to check if the Queue is Empty
 int isEmpty(Queue* q) {
     return q->front == NULL;
 }
 
+
+// Function to insert a new job using enqueue operation
 void enqueue(Queue* q, int id, char* name) {
     Job* newJob = createJob(id, name);
     if (q->rear == NULL) {
@@ -40,6 +48,7 @@ void enqueue(Queue* q, int id, char* name) {
     printf("Job added to the queue: %d - %s\n", id, name);
 }
 
+// Function to remove a job using dequeue operation
 void dequeue(Queue* q) {
     if (isEmpty(q)) {
         printf("Queue is empty, no job to process.\n");
@@ -54,6 +63,7 @@ void dequeue(Queue* q) {
     free(temp);
 }
 
+// Function to display the queue along with values stored in it.
 void displayQueue(Queue* q) {
     if (isEmpty(q)) {
         printf("The queue is empty.\n");
@@ -67,23 +77,29 @@ void displayQueue(Queue* q) {
     }
 }
 
+// Main Function to perform the operations 
 int main() {
     Queue jobQueue;
     initializeQueue(&jobQueue);
 
     int choice, jobId;
     char jobName[50];
+    // Defining the menu
     printf("\nJob Scheduling using Queue (Linked List)\n");
     printf("1. Add Job (Enqueue)\n");
     printf("2. Process Job (Dequeue)\n");
     printf("3. Display Jobs\n");
     printf("4. Exit\n");
     
+    // Forming a loop which only exits when the user chooses exit option
     while (1) {
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        switch (choice) {
+        // Switch statement to process the request of User
+        switch (choice) 
+        {
             case 1:
+            // Adding a job 
                 printf("Enter Job ID: ");
                 scanf("%d", &jobId);
                 printf("Enter Job Name: ");
@@ -91,15 +107,19 @@ int main() {
                 enqueue(&jobQueue, jobId, jobName);
                 break;
             case 2:
+            // Removing a job
                 dequeue(&jobQueue);
                 break;
             case 3:
+            // Displaying all the jobs
                 displayQueue(&jobQueue);
                 break;
             case 4:
+            // Exiting the program 
                 printf("Exiting...\n");
                 exit(0);
             default:
+            // Printing Invalid choice
                 printf("Invalid choice. Please try again.\n");
         }
     }
